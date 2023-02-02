ec66d188900138bd9f1d858458e50b7353e841ae
Luna <110987982+Jianhongkang@users.noreply.github.com>
2023-02-02 16:35:17 +1030

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -3,19 +3,13 @@ import { NextResponse } from "next/server";
 
 export async function middleware(req) {
   const token = await getToken({ req, secret: process.env.JWT_SECRET });
-
-  const { pathname } = req.nextUrl;
-
-  const url = req.nextUrl.clone()
-  url.pathname = '/login'
-
-
+  const { pathname,origin } = req.nextUrl;
+  const url=`${origin}/login`
   if (pathname.includes("/api/auth") || token) {
     return NextResponse.next();
   }
-
-  if (!token && pathname !== "/login") {
-      return NextResponse.redirect(url.pathname)
-  }
+  if (!token && pathname !=='/login') {
+    return NextResponse.redirect(url)
+}
 }
 

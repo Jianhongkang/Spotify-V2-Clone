078d3904660236f2eeaae9957b9a304bc6c73637
Mohsin370 <mohsinijaz13@gmail.com>
2023-02-02 15:04:00 +0500

    middleware redirect code fixed

--- middleware.js
+++ middleware.js
@@ -1,6 +1,10 @@
 import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 
+export const config = {
+  matcher: "/",
+  };
+
 export async function middleware(req) {
   const token = await getToken({ req, secret: process.env.JWT_SECRET });
   const { pathname,origin } = req.nextUrl;
@@ -9,6 +13,10 @@ export async function middleware(req) {
     return NextResponse.next();
   }
   if (!token && pathname !=='/login') {
+    // return NextResponse.redirect(url)
+
+    const url = req.nextUrl.clone();
+    url.pathname = '/login'
     return NextResponse.redirect(url)
 }
 }
